339680584e9d0e8c41ca9b3d448ceb8b84ca287f
Esteban Safranchik <esafranc@cs.brown.edu>
2019-07-30 16:13:31 -0400

    Modified wiser/data/util.py

--- wiser/data/util.py
+++ wiser/data/util.py
@@ -10,8 +10,8 @@ def get_vote_mask(instance):
     mask = np.where(votes == 'ABS', 0, 1)
     return ArrayField(np.ndarray.max(mask, 0))
 
-
 def get_marginals(i, num_tokens, unary_marginals, pairwise_marginals):
+
     unary_marginals_list = []
     pairwise_marginals_list = None if pairwise_marginals is None else []
 
@@ -22,13 +22,48 @@ def get_marginals(i, num_tokens, unary_marginals, pairwise_marginals):
             pairwise_marginals_list.append(pairwise_marginals[i])
         i += 1
 
-    if pairwise_marginals is not None:
-        del pairwise_marginals_list[-1]
-
     return [unary_marginals_list, pairwise_marginals_list, i]
 
+def get_complete_unary_marginals(unary_marginals, gen_label_to_ix, disc_label_to_ix):
 
-def save_label_distribution(save_path, data, unary_marginals=None, pairwise_marginals=None, save_tags=True):
+    if unary_marginals is None or gen_label_to_ix is None or disc_label_to_ix is None:
+        return unary_marginals
+
+    new_unaries = np.zeros((len(unary_marginals), len(disc_label_to_ix)))
+
+    for k, v in disc_label_to_ix.items():
+        if k in gen_label_to_ix:
+            new_unaries[:,v] = unary_marginals[:,gen_label_to_ix[k]-1]
+
+    return new_unaries
+
+def get_complete_pairwise_marginals(pairwise_marginals, gen_label_to_ix, disc_label_to_ix):
+
+    if pairwise_marginals is None or gen_label_to_ix is None or disc_label_to_ix is None:
+        return pairwise_marginals
+
+    new_pairwise = np.zeros((len(pairwise_marginals), len(disc_label_to_ix), len(disc_label_to_ix)))
+
+    for k1, v1 in disc_label_to_ix.items():
+        for k2, v2 in disc_label_to_ix.items():
+            if k1 in gen_label_to_ix and k2 in gen_label_to_ix:
+                new_pairwise[:,v1, v2] = pairwise_marginals[:, gen_label_to_ix[k1]-1, gen_label_to_ix[k2]-1]
+
+    return new_pairwise
+
+
+def save_label_distribution(save_path, data, unary_marginals=None,
+                            pairwise_marginals=None, gen_label_to_ix=None,
+                            disc_label_to_ix=None, save_tags=True):
+
+    unary_marginals = get_complete_unary_marginals(unary_marginals,
+                                                   gen_label_to_ix,
+                                                   disc_label_to_ix)
+
+    pairwise_marginals = get_complete_pairwise_marginals(pairwise_marginals,
+                                                      gen_label_to_ix,
+                                                      disc_label_to_ix)
+
 
     i = 0
     instances = []
@@ -41,6 +76,7 @@ def save_label_distribution(save_path, data, unary_marginals=None, pairwise_marg
 
         if 'tags' in instance and save_tags:
             fields['tags'] =  instance['tags']
+
         if unary_marginals is not None:
             instance_unary_list, instance_pairwise_list, i = get_marginals(
                 i, len(instance_tokens), unary_marginals, pairwise_marginals)
