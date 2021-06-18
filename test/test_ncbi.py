3c4b09a81c3b0f9482e781e32d77d6b7c38ff17f
Yong Zheng Xin <zhengxinyong@Yongs-MBP.fios-router.home>
2021-06-18 19:12:54 -0400

    add docstring

--- test/test_ncbi.py
+++ test/test_ncbi.py
@@ -2,11 +2,17 @@ import pytest
 from allennlp.data import Instance
 
 def test_import_NCBIDiseaseDatasetReader():
+    """
+    pytest: test if we can import NCBIDiseaseDatasetReader
+    """
     from wiser.data.dataset_readers import NCBIDiseaseDatasetReader
     NCBIDiseaseDatasetReader()
     assert True
 
 def get_data():
+    """
+    use NCBIDiseaseDatasetReader to load a dataset
+    """
     from wiser.data.dataset_readers import NCBIDiseaseDatasetReader
     reader = NCBIDiseaseDatasetReader()
     path = "test/toy_data/ncbi.txt"  # assuming we run pytest command on the root wiser folder.
@@ -14,16 +20,27 @@ def get_data():
     return test_data
 
 def test_get_data():
+    """
+    pytest: test if NCBI-Disease data returned from get_data() is not empty
+    """
     data = get_data()
     data = list(data)
     assert data
 
 def test_is_data_instance():
+    """
+    pytest: test if each NCBI-Disease data instanace returned from get_data()
+    is an allennlp.data.Instance
+    """
     data = get_data()
     data = list(data)
     assert isinstance(data[0], Instance)
 
 def test_is_data_instance_not_empty():
+    """
+    pytest: test if each NCBI-Disease data instance returned from get_data()
+    is not an empty instance
+    """
     data = get_data()
     data = list(data)
     assert len(data[0]['tokens'].tokens) and len(data[0]['tags'].labels)
