48caec7f73882b89a6249bd0e0a31ed10db7ea14
Yong Zheng Xin <zhengxinyong@Yongs-MBP.fios-router.home>
2021-06-18 19:18:22 -0400

    unit tests for generative model

---
+++ test/test_generative.py
@@ -0,0 +1,15 @@
+# from wiser.generative import get_label_to_ix, get_rules
+
+def test_import_get_label_to_idx():
+    """
+    pytest: test if we can import get_label_to_ix
+    """
+    from wiser.generative import get_label_to_ix
+    assert True
+
+def test_import_get_rules():
+    """
+    pytest: test if we can import get_rules
+    """
+    from wiser.generative import get_rules
+    assert True
