a3610175d974900c55e6703a12fb55b7ca6e01b4
Yong Zheng Xin <zhengxinyong@Yongs-MBP.fios-router.home>
2021-06-18 19:18:47 -0400

    unit tests for wiser.rules

---
+++ test/test_rules.py
@@ -0,0 +1,28 @@
+# from wiser.rules import TaggingRule, LinkingRule, UMLSMatcher, DictionaryMatcher
+def test_import_TaggingRule():
+    """
+    pytest: test if we can import TaggingRule
+    """
+    from wiser.rules import TaggingRule
+    assert True
+
+def test_import_LinkingRule():
+    """
+    pytest: test if we can import LinkingRule
+    """
+    from wiser.rules import LinkingRule
+    assert True
+
+def test_import_UMLSMatcher():
+    """
+    pytest: test if we can import UMLSMatcher
+    """
+    from wiser.rules import UMLSMatcher
+    assert True
+
+def test_import_DictionaryMatcher():
+    """
+    pytest: test if we can import DictionaryMatcher
+    """
+    from wiser.rules import DictionaryMatcher
+    assert True
